da814a0ba7dff7886d9871530e4e3339cacf6006
Jingbei Li <i@jingbei.li>
2018-04-15 19:13:29 +0800

    added object_to_dict function

--- django_orm.py
+++ django_orm.py
@@ -26,3 +26,16 @@ apps.populate(settings.INSTALLED_APPS)
 def migrate():
     execute_from_command_line(['', 'makemigrations', module_name])
     execute_from_command_line(['', 'migrate', module_name])
+
+def object_to_dict(instance):
+    opts = instance._meta
+    data = {}
+    for f in opts.concrete_fields + opts.many_to_many:
+        if isinstance(f, ManyToManyField):
+            if instance.pk is None:
+                data[f.name] = []
+            else:
+                data[f.name] = list(f.value_from_object(instance).values_list('pk', flat=True))
+        else:
+            data[f.name] = f.value_from_object(instance)
+    return data
